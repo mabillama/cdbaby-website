d9ea15e0c0442e29d68a1c79cc3d9369e4e07a41
Marc Abillama <mabillama@gmail.com>
2022-10-20 22:49:49 +0200

    Creates basic tailwind color configuration for the site

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ebebeb",
+      black: "#1f1f30",
+      yellow: "#f9e606",
+      orange: "#f2b538",
+      red: "#f74040",
+      blue: "#2b38b0",
+      cyan: "#40bcd0",
+      pink: "#f57474",
+    },
+  },
+  plugins: [],
+};
